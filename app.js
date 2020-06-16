93aa07547c6ae73a3a2910abe50bd885b7b4e3c8
vatsalvatsyayan <43917696+vatsalvatsyayan@users.noreply.github.com>
2020-06-16 10:21:55 +0530

    v3
    connected to database

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 var express = require('express');
+var mongoose = require('mongoose')
 var app = express();
 var request = require('request');
 app.set("view engine", "ejs");
 
 app.use('/assets',express.static('assets'));
 
+mongoose.connect('mongodb+srv://Vatsal:test@cluster0-rqxu9.mongodb.net/MovieList?retryWrites=true&w=majority')
+
+var movieSchema = new mongoose.Schema({
+    Title: String,
+    Year: String,
+    Runtime: String,
+    imdbRating: String
+});
+
+var completed = mongoose.model('completed',movieSchema);
+var toWatch  = mongoose.model('toWatch',movieSchema);
+
 var bodyParser = require('body-parser');
 
-var completed = [];
+
 var towatch = [];
 
 
@@ -43,11 +56,7 @@ app.post('/info',urlencoded, function(req,res){
     request(req.body.movielink,function(error, response, body){
         if(!error && response.statusCode >= 200){
             var data = JSON.parse(body);
-
-            
-           
-
-            res.render("info",{data:data});
+             res.render("info",{data:data });
         }
     })
 
@@ -60,22 +69,34 @@ app.post('/completed',urlencoded, function(req,res){
         if(!error && response.statusCode >= 200){
             var data = JSON.parse(body);
 
+            var complete = {
+                Title: data.Title,
+                Year: data.Year,
+                Runtime: data.Runtime,
+                imdbRating: data.imdbRating
+            }
+
+            var addToCompleted = completed(complete).save(function(err,completed){
+                if(err) throw err;
+                res.render("search");
+
+            })
             
-            completed.push(data);
-            console.log("After this")
-            console.log(completed);
+            
+        
 
             
-            console.log(data);
-
-            res.render("completed",{data:data,user:completed});
         }
     })
 
 });
 
 app.get('/completed',function(req,res){
-    res.render("completed",{user:completed});
+    completed.find({}, function(err,completed){
+        if(err) throw err;
+        res.render("completed",{user:completed});
+    });
+    
 })
 
 app.post('/towatch',urlencoded, function(req,res){
@@ -86,20 +107,28 @@ app.post('/towatch',urlencoded, function(req,res){
             var data = JSON.parse(body);
 
             
-            towatch.push(data);
-            
+            var pending = {
+                Title: data.Title,
+                Year: data.Year,
+                Runtime: data.Runtime,
+                imdbRating: data.imdbRating
+            }
 
-            
-            
+            var addToWatch = toWatch(pending).save(function(err,completed){
+                if(err) throw err;
+                res.render("search");
 
-            res.render("towatch",{data:data,user:towatch});
+            })
         }
     })
 
 });
 
 app.get('/towatch',function(req,res){
-    res.render("towatchget",{user:towatch});
+    toWatch.find({}, function(err,towatch){
+        if(err) throw err;
+        res.render("towatch",{user:towatch});
+    });
 })
 
 
